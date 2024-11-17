be0c34ed57dc8a994419657680b8456a2bdf2a49
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-11-17 14:56:27 +0100

    build: adjust build configs to support fallbacks in case a node specific variable is accessed in the browser
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,43 @@
 const path = require('path');
+const webpack = require('webpack');
 
-module.exports = {
-  entry: './src/index.ts',
-  output: {
-    filename: 'index.debug.js',
-    path: path.resolve(__dirname, 'dist'),
-    library: {
-      name: 'Flytrap',
-      type: 'umd',
-      export: 'default',
-    },
-    umdNamedDefine: true,
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-    fallback: {
-      fs: false,
-      path: require.resolve("path-browserify")
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: [/node_modules/, /tests/],
+module.exports = [
+  {
+    entry: './src/index.ts',
+    output: {
+      filename: 'index.js',
+      path: path.resolve(__dirname, 'dist'),
+      library: {
+        name: 'flytrap',
+        type: 'umd',
+        export: 'default',
       },
+      umdNamedDefine: true,
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+      fallback: {
+        buffer: require.resolve('buffer/'),
+        process: require.resolve('process/browser'),
+        fs: false,
+        path: require.resolve('path-browserify'),
+      },
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+        process: 'process/browser',
+      }),
     ],
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: 'ts-loader',
+          exclude: [/node_modules/, /tests/],
+        },
+      ],
+    },
+    mode: 'production',
   },
-  mode: 'development', // Sets Webpack to output unminified code
-  devtool: 'source-map', // Generates source maps for easier debugging
-};
+];
