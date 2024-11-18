84b24bf1d742822adb50c6f98b0ff21ccfeb6afc
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-11-18 18:29:57 +0100

    chore: remove log statement
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- src/utils/ipInfo.ts
+++ src/utils/ipInfo.ts
@@ -3,7 +3,6 @@ import axios from 'axios';
 export const getIpAddress = async () => {
   try {
     const { data }= await axios.get('https://api.ipify.org?format=json');
-    console.log('ip data: ', data)
     return data.ip;
   } catch {
     return 'Unknown';
